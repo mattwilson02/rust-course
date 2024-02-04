e099ff3fd570ae49dc26a8575e5d24695dfe118d
mattwilson02 <mattwilsonbmx@gmail.com>
2024-02-04 15:39:56 +0000

    chore: run cargo create command

--- src/main.rs
+++ src/main.rs
@@ -55,8 +55,6 @@
 //     println!("Success");
 // }
 
-// Destructuring
-
 // fn main() {
 //     let (x, y);
 
@@ -67,3 +65,39 @@
 
 //     println!("Success");
 // }
+
+// fn main() {
+//     let x: u8 = 127;
+
+//     println!("{}", x)
+// }
+
+// fn main() {
+//     let v: u16 = 38_u8 as u16;
+
+//     print!("Success")
+// }
+
+// fn main() {
+//     let x: i32 = 10;
+
+//     assert_eq!("i32".to_string(), type_of(&x));
+// }
+
+// fn type_of<T>(_: &T) -> String {
+//     format!("{}", std::any::type_name::<T>())
+// }
+
+// fn main() {
+//     assert_eq!(i8::MAX, 127);
+//     assert_eq!(u8::MAX, 255);
+
+//     print!("Success")
+// }
+
+fn main() {
+    let v1: u16 = 251_u16 + 8;
+    let v2 = i16::checked_add(251, 10).unwrap();
+
+    println!("{}, {}", v1, v2)
+}
