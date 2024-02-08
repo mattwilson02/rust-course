f400b4f8723835bb16e072d95a4febbaaf99fe4a
mattwilson02 <mattwilsonbmx@gmail.com>
2024-02-08 15:40:17 +0000

    section: numbers

--- src/numbers/mod.rs
+++ src/numbers/mod.rs
@@ -95,9 +95,31 @@
 //     print!("Success")
 // }
 
-pub fn numbers_fn() {
-    let v1: u16 = 251_u16 + 8;
-    let v2: i16 = i16::checked_add(251, 10).unwrap();
+// pub fn numbers_fn() {
+//     let v1: u16 = 251_u16 + 8;
+//     let v2: i16 = i16::checked_add(251, 10).unwrap();
 
-    println!("{}, {}", v1, v2)
+//     println!("{}, {}", v1, v2)
+// }
+
+// pub fn numbers_fn() {
+//     let mut sum = 0;
+
+//     for i in -3..2 {
+//         sum += i;
+//     }
+
+//     assert!(sum == -5);
+
+//     for c in 'a'..='z' {
+//         println!("{}", c);
+//     }
+// }
+
+use std::ops::{Range, RangeInclusive};
+
+pub fn numbers_fn() {
+    assert_eq!((1..5), Range { start: 1, end: 5 });
+
+    assert_eq!((1..=5), RangeInclusive::new(1, 5))
 }
